1582869b1c31a8f0fe330fe94504d68a5da537d1
Kateryna Tkachenko <katherinatkachenko@gmail.com>
2018-10-28 11:52:10 +0200

    Fixed counter

--- src/App.js
+++ src/App.js
@@ -2,27 +2,60 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Application extends React.Component {
+  state = {
+    counter: 1,
+  }
+
+  increaseCounter = () => this.setState(({ counter }) => ({ counter: counter + 1 }));
+  decreaseCounter = () => this.setState(({ counter }) => ({ counter: counter - 1 }));
+
+   NewIncreaseCounter = () => this.setState(({ counter }) => ({ counter: counter + 5 }));
+   NewDecreaseCounter = () => this.setState(({ counter }) => ({ counter: counter -5 }));
+
+handleIncreaseClick = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    
+   this.increaseCounter();
+  }
+handleNewIncreaseClick = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    
+   this.NewIncreaseCounter();
+  }
+  
+  handleDecreaseClick = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    
+    this.decreaseCounter();
+    
+  }
+  handleNewDecreaseClick = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    
+    this.NewDecreaseCounter();
+    
+  }
   render() {
+    const { counter } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>{counter}</h1>
+        <p>Increase or decrease your counter</p>
+        <p>
+          <a href="#" onClick={this.handleIncreaseClick}>Increase counter</a>
+          <a href="#" onClick={this.handleDecreaseClick}>Decrease counter</a>
+          <a href="#" onClick={this.handleNewIncreaseClick}>+5</a>
+          <a href="#" onClick={this.handleNewDecreaseClick}>-5</a>
+        </p>
       </div>
     );
   }
 }
 
-export default App;
+export default Application;
--- src/index.js
+++ src/index.js
@@ -1,66 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Application from './App';
 import * as serviceWorker from './serviceWorker';
 
-class Application extends React.Component {
-    state = {
-      counter: 1,
-    }
   
-    increaseCounter = () => this.setState(({ counter }) => ({ counter: counter + 1 }));
-    decreaseCounter = () => this.setState(({ counter }) => ({ counter: counter - 1 }));
-  
-     NewIncreaseCounter = () => this.setState(({ counter }) => ({ counter: counter + 5 }));
-     NewDecreaseCounter = () => this.setState(({ counter }) => ({ counter: counter -5 }));
-  
-  handleIncreaseClick = e => {
-      e.preventDefault();
-      e.stopPropagation();
-      
-     this.increaseCounter();
-    }
-  handleNewIncreaseClick = e => {
-      e.preventDefault();
-      e.stopPropagation();
-      
-     this.NewIncreaseCounter();
-    }
-    
-    handleDecreaseClick = e => {
-      e.preventDefault();
-      e.stopPropagation();
-      
-      this.decreaseCounter();
-      
-    }
-    handleNewDecreaseClick = e => {
-      e.preventDefault();
-      e.stopPropagation();
-      
-      this.NewDecreaseCounter();
-      
-    }
-    render() {
-      const { counter } = this.state;
-  
-      return (
-        <div>
-          <h1>{counter}</h1>
-          <p>Increase or decrease your counter</p>
-          <p>
-            <a href="#" onClick={this.handleIncreaseClick}>Increase counter</a>
-            <a href="#" onClick={this.handleDecreaseClick}>Decrease counter</a>
-            <a href="#" onClick={this.handleNewIncreaseClick}>+5</a>
-            <a href="#" onClick={this.handleNewDecreaseClick}>-5</a>
-          </p>
-        </div>
-      );
-    }
-  }
-  
-  ReactDOM.render(<Application />, document.getElementById('root'));
+ReactDOM.render(<Application />, document.getElementById('root'));
 
 
 serviceWorker.unregister();
