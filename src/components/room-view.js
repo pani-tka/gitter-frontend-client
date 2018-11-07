4e063297eb79143859e03e21136a517bdbc4c139
Kateryna Tkachenko <katherinatkachenko@gmail.com>
2018-11-07 19:56:51 +0200

    current changes

---
+++ src/components/room-view.js
@@ -0,0 +1,23 @@
+import React, {Component}from 'react';
+import styles from './room-view.module.scss';
+
+
+class RoomView extends Component {
+
+    render() {
+        const { selectedRoom} = this.props;
+
+       if (selectedRoom){
+           return (
+            <div className={styles.container}>
+            <img className={styles.image} src={selectedRoom.avatarUrl} alt=""/>
+            <a className={styles.name}  href="#" > {selectedRoom.name} </a>
+          </div>
+           )
+        }
+        
+        return <p>Please select a room to start messaging</p>
+    }
+}
+
+export default RoomView;
