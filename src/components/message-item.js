f938b54adc43141641da6e5858c47993c60b9947
Kateryna Tkachenko <katerynatkachenko@MacBook-Pro-Kateryna.local>
2018-11-20 20:38:07 +0200

    Added date and time for messages

--- src/components/message-item.js
+++ src/components/message-item.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { DateTime } from 'luxon';
 import styles from './message-item.module.scss';
 
 class MessageItem extends Component {
@@ -11,14 +12,20 @@ class MessageItem extends Component {
     const {
       text,
       fromUser,
+      sent,
     } = this.props;
-
+  
+    const sentTime = DateTime.fromISO(sent).setLocale('en').toFormat('LL LLL, t'); 
+    
   return (
     <div className={styles.container}>
       <img className={styles.avatar} src={fromUser.avatarUrl} alt=''/>
       <a className={styles.username} href='#' onClick={this.handleClick}>
         {fromUser.displayName}
       </a>
+      <span className={styles.time}>
+        {sentTime}
+      </span>
       <span className={styles.text}>
         {text}
       </span>
