eaa10562373797974aee38e778747eed1cb0cedb
Kateryna Tkachenko <katerynatkachenko@MacBook-Pro-Kateryna.local>
2018-11-20 23:18:00 +0200

    Added Redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { reducer } from "./reducer";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const appElement = document.getElementById("app");
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  appElement);
 
 serviceWorker.unregister();
